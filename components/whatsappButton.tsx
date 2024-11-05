ee9d05c787218d4c7967b8aa4ebea24bf123d0d2
pikachupradeep <101954763+pikachupradeep@users.noreply.github.com>
2024-11-05 11:51:26 +0530

    first commit

--- components/whatsappButton.tsx
+++ components/whatsappButton.tsx
@@ -1,19 +1,14 @@
 
 
-import Link from 'next/link';
-
 const WhatsAppButton = () => {
-    const phoneNumber = process.env.NEXT_PUBLIC_WHATSAPP_PHONE; // Use NEXT_PUBLIC_ prefix to expose it to the client
-    const message = 'Hello! I would like to chat.';
-
-    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+   
 
     return (
-        <Link href={whatsappUrl} passHref>
-            <button style={{ padding: '10px 20px', backgroundColor: '#25D366', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
-                Chat on WhatsApp
-            </button>
-        </Link>
+        <a href="tel:0770099687" className="bg-green-500 text-white px-6 py-3 rounded-lg text-xl font-semibold shadow-lg hover:bg-green-600 transition duration-300 ease-in-out">
+        Call Us Now
+        </a>
+        
+       
     );
 };
 
